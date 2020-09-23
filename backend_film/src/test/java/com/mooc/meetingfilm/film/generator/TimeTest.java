9d8891439e25ffcb059eea62b8664e118e4540f8
oweson <570347720@qq.com>
2020-09-23 21:31:14 +0800

    docs: 修改readme

---
+++ backend_film/src/test/java/com/mooc/meetingfilm/film/generator/TimeTest.java
@@ -0,0 +1,15 @@
+package com.mooc.meetingfilm.film.generator;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author chengwanli
+ * @date 2020/9/15 22:36
+ */
+
+
+public class TimeTest {
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+}
